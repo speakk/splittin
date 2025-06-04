2ed0c761de824c3241e7a8a1930a2f79d8375401
speak <speakk@gmail.com>
2025-06-04 09:27:33 +0300

    Gun gizmo, ammo comes from gun

--- src/in_game/player.rs
+++ src/in_game/player.rs
@@ -1,9 +1,11 @@
-use std::f32::consts::PI;
 use crate::in_game::ammo_ball::{AmmoBall, InitialVelocity};
 use crate::in_game::input::{PlayerInputContext, Shoot};
 use bevy::prelude::*;
 use bevy_enhanced_input::events::Started;
 use bevy_enhanced_input::prelude::Actions;
+use std::f32::consts::PI;
+use bevy::color::Color::Srgba;
+use bevy::color::palettes::basic::RED;
 
 #[derive(Component)]
 pub struct Player;
@@ -13,11 +15,17 @@ pub(super) fn player_plugin(app: &mut App) {
     app.add_observer(react_to_shoot);
 }
 
+const GUN_LENGTH: f32 = 100.0;
+
 fn observe_add_player(
     trigger: Trigger<OnAdd, Player>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut gizmo_assets: ResMut<Assets<GizmoAsset>>,
 ) {
+    let mut gizmo = GizmoAsset::default();
+    gizmo.line_2d(Vec2::ZERO, Vec2::new(0.0, -GUN_LENGTH), Color::srgb(0.8, 0.6, 0.4));
+
     commands.entity(trigger.target()).insert((
         Sprite {
             image: asset_server.load("player_ball.png"),
@@ -25,6 +33,14 @@ fn observe_add_player(
             ..Default::default()
         },
         Actions::<PlayerInputContext>::default(),
+        children![Gizmo {
+            handle: gizmo_assets.add(gizmo),
+            line_config: GizmoLineConfig {
+                width: 20.0,
+                ..Default::default()
+            },
+            ..Default::default()
+        }],
     ));
 }
 
@@ -42,6 +58,6 @@ fn react_to_shoot(
     commands.spawn((
         AmmoBall,
         InitialVelocity(initial_velocity),
-        Transform::from_translation(position),
+        Transform::from_translation(position + (Vec2::from_angle(rotation) * GUN_LENGTH).extend(0.0)),
     ));
 }
