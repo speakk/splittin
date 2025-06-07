85555a5f08e937bd8bc02a6a1180a491cdd5d419
speak <speakk@gmail.com>
2025-06-07 14:41:10 +0300

    Zoom out a tad

--- src/in_game/camera.rs
+++ src/in_game/camera.rs
@@ -2,6 +2,16 @@ use bevy::prelude::*;
 
 pub(super) fn camera_plugin(app: &mut App) {
     app.add_systems(Startup, |mut commands: Commands| {
-        commands.spawn(Camera2d::default());
+        commands.spawn((
+            Camera2d,
+            Camera {
+                hdr: true, // HDR is required for the bloom effect
+                ..default()
+            },
+            Projection::Orthographic(OrthographicProjection {
+                scale: 2.0,
+                ..OrthographicProjection::default_2d()
+            }),
+        ));
     });
 }
